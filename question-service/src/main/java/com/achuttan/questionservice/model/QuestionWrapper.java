0d4b6edc862579b2891ab247f0b3e34e67925e13
Achuttan Jiju <ajdsparkler@gmail.com>
2023-10-30 10:48:48 +0530

    inital commit

---
+++ question-service/src/main/java/com/achuttan/questionservice/model/QuestionWrapper.java
@@ -0,0 +1,19 @@
+package com.achuttan.questionservice.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class QuestionWrapper {
+	
+	
+	
+	private Integer id;
+	private String questionTitle;
+	private String option1;
+	private String option2;
+	private String option3;
+	private String option4;
+
+}
